3b3786a0772b0e6af2f201f6984ffead5ce691a7
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-04-23 16:56:53 +0200

    Exercise 18.3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,11 +1,13 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +19,16 @@ public class DbService {
         return taskRepository.findAll();
     }
 
-    public Task getTaskById(Long id) {
-        return taskRepository.findById(id).orElse(null);
+    public Task getTask(final Long id) throws TaskNotFoundException {
+        return taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
+    }
+
+    public Task saveTask(final Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(final Long id) {
+        taskRepository.deleteById(id);
     }
 
 }
